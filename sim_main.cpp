3b227555cab8411379ea2eb870d9aa2e2cf2164d
Elodiedb <elodie.de-brito@ensta-paris.fr>
2025-03-11 11:55:15 +0100

    Vérification des échecs, détection du mat, ajout du nombre de case contrôlées dans éval, affichage des coups joues
    Ajout d'un commentaire dans nb_cases_ctrl
    Correction d'un bug dans nb_cases_ctrl (et de sa signature, l'argument est de nouveau const)
    Ajout d'un tableau de poids pour nb_cases_ctrl
    nb_cases_ctrl devient pair pour distinguer protégé de vides contrôlées
    Décommenter les tables
    retrait du cas où abs(eval >6 dans minmax
    ajout de vérif dans prof suivante pour s'assurer que le coup ne provoque pas un échec sur notre roi
    création de opérateur << pour coup (dans chess.hpp et chess.cpp)
    Ajout d'un historique et de la possibilité d'annuler un coup
    création d'une classe historique
    Ajout d'un include vector dans chess.hpp
    Ajout de l'affichage des coups
    Abandon de l'idée d'historique
    Ajout dans le main d'une option pour tester le mat

--- sim_main.cpp
+++ sim_main.cpp
@@ -7,28 +7,42 @@ using namespace std;
 int main()
 {
     Echiquier e = Echiquier();
-    int prof = 4;
+    int prof = 2;
+    int n = 0;
     char *coup_alg;
-    cout << e;
+    coup c = coup();
+    Historique h = Historique();
+    //cout << e;
     cout << "eval : " << eval(e) << "\n";
     // cout << "Entrez un coup";
     Position pos = Position(&e, true, nullptr, nullptr, coup(), nullptr, nullptr);
     int i = 1;
-    while (i < 100)
+    Position test_mat = Position(pos.e, pos.white_turn, nullptr, nullptr,coup(), nullptr, nullptr);
+    test_mat.prof_suivante(1);
+    int tmax = 400;
+    while (i < tmax && test_mat.fille != nullptr)     //permet de détecter le mat
     {
         //cout << "bcgr : " << (e.bcgr ? "True" : "False") << "\n";
-        if (true)
+        if (false)     //true pour ordi vs ordi, false pour JvJ, pos.white_turn pour ordi sur les blancs, !pos.white_turn pour ordi sur les noirs
         {
-            cout << "tour : " << i << "\n";
+            cout << *(pos.e);
+            cout << "tour : " << (i+1) / 2 << "\n";
             // pos.prof_suivante(prof);
             // cout << "prof_suivante generee \n";
-            coup c = coup();
+            c = coup();
             // cout << "coup par defaut genere";
             meilleur_coup mc = alpha_beta(pos, prof, -1000, +1000, coup());
+            type_piece* ptr_type = nullptr;
+            if (pos.e->board[c.arr.i][c.arr.j] != nullptr)
+            {
+                ptr_type = new type_piece(pos.e->board[c.arr.i][c.arr.j]->type);
+            }
             cout << "eval estimee :" << mc.evaluation << "\n";
+            cout << "Coup joue : " << mc.c << endl;
             pos.e->make_move(mc.c);
+            h.update(mc.c, ptr_type, *pos.e);
             // cout << "pos.white_turn : " << (pos.white_turn ? "true" : "false") << "\n";
-            cout << *(pos.e);
+            //cout << *(pos.e);
             cout << "eval : " << eval(*pos.e) << "\n";
             // pos = Position(pos.e, ! pos.white_turn, nullptr, nullptr, coup());      //A remplacer par nullptr/ destroy ?
             pos.white_turn = !pos.white_turn;
@@ -39,31 +53,94 @@ int main()
             // cout << "fin de boucle\n";
             // pos.white_turn = !pos.white_turn;
             // pos.destroy();
-            i++;
         }
         else
         {
-            coup c = coup();
-            while(!is_legal(c,*pos.e))
+            c = coup();
+            bool unmake = false;
+            bool valide = false;
+            while( !(unmake || valide))
             //tour des noirs aka lae joueur.euse
             //cout << "e avant c_in\n" << pos.e;
             {
+                cout << *(pos.e);
                 cout << "Entrez un coup : ";
                 char alg[10];
-                for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
                 {
-                    alg[i] = ' ';
+                    alg[j] = ' ';
                 }
                 //cout << "e juste avant c_in\n" << pos.e;
                 cin >> alg;
                 //cout << "e_juste après le c_in\n" << pos.e;
                 c = alg2coup(alg, pos.white_turn, *pos.e);
+                if (c.p.type == K && c.dep.i == c.arr.i && c.dep.j == c.arr.j && i >= c.dep.i + 1)
+                {
+                    //Ecrire Kxy-xy pour x une colonne (dans {a,b,..., h} et y un nombre entre 1 et 8 annulera les y derniers coups
+                    unmake =true;
+                    valide = true;
+                    n = c.arr.i;
+                }
+                if (is_legal(c, *pos.e))
+                {
+                    char val[10];
+                    for (int j = 0; j < 10; j++)
+                    {
+                        val[j] = ' ';
+                    }
+                    type_piece* ptr_type = nullptr;
+                    if (pos.e->board[c.arr.i][c.arr.j] != nullptr)
+                    {
+                        ptr_type = new type_piece(pos.e->board[c.arr.i][c.arr.j]->type);
+                    }
+                    h.update(c, ptr_type, *pos.e);
+                    pos.e->make_move(c);
+                    cout << *pos.e;
+                    cout << "Valider le coup ? (y/n)";
+                    cin >> val;
+                    if (val[0] == 'y')
+                    {
+                        valide = true;
+                    }
+                    else
+                    {
+                        //cout << "Dans le else\n";
+                        valide = false;
+                        go_back(*pos.e, 1, i, h);
+                        h.go_back(1);
+                    }
+                    //cout << (valide ? "True" : "false");
+                }
                 //cout << "lecture terminee\n";
                 //cout << "e avant coup\n" << pos.e;
             }
-            pos.e->make_move(c);
-            pos.white_turn = !pos.white_turn;
-            cout << *(pos.e);
+            //cout << (valide ? "True" : "false");
+            //cout << "Hors du while";
+            if (unmake)
+            {
+                go_back(*pos.e, n, i-1, h);
+                h.go_back(n);
+                if (n%2 == 1)
+                {
+                    pos.white_turn = !pos.white_turn;
+                    i = i - n ;
+                }
             }
+            else
+            {
+                pos.white_turn = !pos.white_turn;
+            }
+        }
+        i++;
+        test_mat = Position(pos.e, pos.white_turn, nullptr, nullptr,coup(), nullptr, nullptr);
+        test_mat.prof_suivante(1);
+    }
+    if (i!= tmax)
+    {
+        //La partie s'est arrêtée avant d'atteindre le nombre maximal de tour : il y a eu pat ou mat
+        if ((pos.e)->ischeck(pos.white_turn))
+            {cout << (pos.white_turn ? "Les noirs ont gagne!!" : "Les blancs ont gagne!!");}
+        else
+            {cout << "C'est un pat";}
     }
 };
