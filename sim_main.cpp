c36226f46694d0b4e7df990f68b0eafbc66bd5d9
Elodiedb <elodie.de-brito@ensta-paris.fr>
2025-03-17 08:41:21 +0100

    Suppression des commentaires superflus et ajout de commentaires
    Suppression des commentaires superflus et ajout de commentaires

--- sim_main.cpp
+++ sim_main.cpp
@@ -12,9 +12,7 @@ int main()
     char *coup_alg;
     coup c = coup();
     Historique h = Historique();
-    //cout << e;
     cout << "eval : " << eval(e) << "\n";
-    // cout << "Entrez un coup";
     Position pos = Position(&e, true, nullptr, nullptr, coup(), nullptr, nullptr);
     int i = 1;
     Position test_mat = Position(pos.e, pos.white_turn, nullptr, nullptr,coup(), nullptr, nullptr);
@@ -22,46 +20,31 @@ int main()
     int tmax = 400;
     while (i < tmax && test_mat.fille != nullptr)     //permet de détecter le mat
     {
-        //cout << "bcgr : " << (e.bcgr ? "True" : "False") << "\n";
         if (false)     //true pour ordi vs ordi, false pour JvJ, pos.white_turn pour ordi sur les blancs, !pos.white_turn pour ordi sur les noirs
         {
+            // L'ordi joue ce coup
             cout << *(pos.e);
             cout << "tour : " << (i+1) / 2 << "\n";
-            // pos.prof_suivante(prof);
-            // cout << "prof_suivante generee \n";
             c = coup();
-            // cout << "coup par defaut genere";
             meilleur_coup mc = alpha_beta(pos, prof, -1000, +1000, coup());
             type_piece* ptr_type = nullptr;
             if (pos.e->board[c.arr.i][c.arr.j] != nullptr)
             {
+                // L'ordi vient de jouer une capture
                 ptr_type = new type_piece(pos.e->board[c.arr.i][c.arr.j]->type);
             }
-            cout << "eval estimee :" << mc.evaluation << "\n";
-            cout << "Coup joue : " << mc.c << endl;
             pos.e->make_move(mc.c);
             h.update(mc.c, ptr_type, *pos.e);
-            // cout << "pos.white_turn : " << (pos.white_turn ? "true" : "false") << "\n";
-            //cout << *(pos.e);
             cout << "eval : " << eval(*pos.e) << "\n";
-            // pos = Position(pos.e, ! pos.white_turn, nullptr, nullptr, coup());      //A remplacer par nullptr/ destroy ?
             pos.white_turn = !pos.white_turn;
-            // cout << "opposition du tour accomplie";
-            // delete &pos;
-            // cout << "delete accompli\n";
-            // Position pos= Position(e, true, nullptr, nullptr, coup());
-            // cout << "fin de boucle\n";
-            // pos.white_turn = !pos.white_turn;
-            // pos.destroy();
         }
         else
         {
+            // L'ordi ne joue pas ce coup
             c = coup();
             bool unmake = false;
             bool valide = false;
             while( !(unmake || valide))
-            //tour des noirs aka lae joueur.euse
-            //cout << "e avant c_in\n" << pos.e;
             {
                 cout << *(pos.e);
                 cout << "Entrez un coup : ";
@@ -70,9 +53,7 @@ int main()
                 {
                     alg[j] = ' ';
                 }
-                //cout << "e juste avant c_in\n" << pos.e;
                 cin >> alg;
-                //cout << "e_juste après le c_in\n" << pos.e;
                 c = alg2coup(alg, pos.white_turn, *pos.e);
                 if (c.p.type == K && c.dep.i == c.arr.i && c.dep.j == c.arr.j && i >= c.dep.i + 1)
                 {
@@ -83,7 +64,7 @@ int main()
                 }
                 if (is_legal(c, *pos.e))
                 {
-                    char val[10];
+                    char val[10];        // Chaine de caractère permettant la validation du coup par l'utilisateur
                     for (int j = 0; j < 10; j++)
                     {
                         val[j] = ' ';
@@ -100,24 +81,21 @@ int main()
                     cin >> val;
                     if (val[0] == 'y')
                     {
+                        // L'utilisateur a validé (y pour 'yes')
                         valide = true;
                     }
                     else
                     {
-                        //cout << "Dans le else\n";
+                        // L'utilisateur n'a pas validé le coup, on l'annule et on en demande un nouveau
                         valide = false;
                         go_back(*pos.e, 1, i, h);
                         h.go_back(1);
                     }
-                    //cout << (valide ? "True" : "false");
                 }
-                //cout << "lecture terminee\n";
-                //cout << "e avant coup\n" << pos.e;
             }
-            //cout << (valide ? "True" : "false");
-            //cout << "Hors du while";
             if (unmake)
             {
+                // La commande était de la forme Kxy-xy
                 go_back(*pos.e, n, i-1, h);
                 h.go_back(n);
                 if (n%2 == 1)
@@ -128,6 +106,7 @@ int main()
             }
             else
             {
+                // Un coup a été joué
                 pos.white_turn = !pos.white_turn;
             }
         }
